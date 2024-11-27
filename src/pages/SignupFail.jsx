221246683ab63542a3d3e11ba4594bc59e8e07e7
eastpillar <129244821+eastpillar@users.noreply.github.com>
2024-11-27 20:40:14 +0900

    js파일들 jsx파일로 변경

---
+++ src/pages/SignupFail.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const SignupFail = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('/'); 
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate('/');
+  }
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      maxWidth: '400px',
+      margin: '50px auto',
+      padding: '30px',
+      borderRadius: '15px',
+      boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+      backgroundColor: '#ffffff',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontFamily: "'Noto Sans KR', sans-serif"
+    }}>
+      <img src="/images/redFail.png" style={{ maxwidth: '100%', maxHeight: '100%', objectFit: 'contain' }} />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label style={{ display: 'block', textAlign: 'center' }}>
+            회원가입에 실패했습니다.
+          </label>
+        </div>
+        <button type="submit"
+        onClick={handleLoginClick}
+          style={{
+            width: '100%',
+            padding: '12px',
+            backgroundColor: '#3b82f6',
+            color: 'white',
+            border: 'none',
+            borderRadius: '5px',
+            fontSize: '16px',
+            fontWeight: 'bold',
+            cursor: 'pointer',
+            transition: 'background-color 0.3s'
+          }}>로그인 페이지</button>
+
+      </form>
+    </div>
+  );
+}
+
+export default SignupFail;
